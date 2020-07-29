bc3a287e7a445dcdeeb4dee8db9f7ec7dec9e508
imerofeev <imerofeev@gmail.com>
2020-07-29 13:25:58 +0300

    Личный проект: надо подкачаться (часть 1)

--- js/data.js
+++ js/data.js
@@ -1,53 +1,12 @@
 'use strict';
 
 (function () {
-  var TYPEHOUSE = ['palace', 'flat', 'house', 'bungalo'];
-  var TIME = ['12:00', '13:00', '14:00'];
-  var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-  var TITLE = ['title-1', 'title-2', 'title-3', 'title-4', 'title-5', 'title-6', 'title-7', 'title-8'];
-  var DESCRIPTION = ['description-1', 'description-2', 'description-3', 'description-4', 'description-5', 'description-6', 'description-7', 'description-8'];
   var MAIN_PIN_CIRCLE = 65;
   var MAIN_PIN_HALF_CIRCLE = 33;
-  var X_OTHER_PIN = 50;
-  var Y_OTHER_PIN = 70;
 
   var map = document.querySelector('.map');
   var mapPinMain = map.querySelector('.map__pin--main');
   var adForm = document.querySelector('.ad-form');
-
-  function getCards(numberOfCards) {
-    var cards = [];
-    var location;
-
-    for (var i = 0; i < numberOfCards; i++) {
-      location = {
-        x: window.utils.getRandElement(0, 1200 - (X_OTHER_PIN / 2)),
-        y: window.utils.getRandElement(130 - Y_OTHER_PIN, 630 - Y_OTHER_PIN)
-      };
-
-      cards[i] = {
-        author: {
-          avatar: 'img/avatars/user0' + (i + 1) + '.png'
-        },
-        offer: {
-          title: window.utils.getRandElementArr(TITLE),
-          address: (location.x + (X_OTHER_PIN / 2)) + ', ' + (location.y + Y_OTHER_PIN),
-          price: window.utils.getRandElement(1000, 100000),
-          type: window.utils.getRandElementArr(TYPEHOUSE),
-          rooms: window.utils.getRandElement(1, 3),
-          guests: window.utils.getRandElement(1, 3),
-          checkin: window.utils.getRandElementArr(TIME),
-          checkout: window.utils.getRandElementArr(TIME),
-          features: window.utils.getRandLengthArr(FEATURES),
-          description: window.utils.getRandElementArr(DESCRIPTION),
-          photos: window.utils.getRandLengthArr(PHOTOS)
-        },
-        location: location
-      };
-    }
-    return cards;
-  }
 
   function getAddress(xPin, yPin) {
     var inputAddress = adForm.querySelector('#address');
@@ -69,7 +28,6 @@
   getAddress(MAIN_PIN_CIRCLE, MAIN_PIN_HALF_CIRCLE);
 
   window.data = {
-    getCards: getCards,
     getAddress: getAddress,
   };
 })();
---
+++ js/xhr.js
@@ -0,0 +1,47 @@
+'use strict';
+
+(function () {
+  var StatusCode = {
+    OK: 200
+  };
+
+  var Url = {
+    GET_CARDS: 'https://javascript.pages.academy/keksobooking/data'
+  };
+
+  var Method = {
+    GET: 'GET',
+    POST: 'POST'
+  };
+
+  var TIMEOUT_IN_MS = 10000;
+  function load(url, method, onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.open(method, url);
+    xhr.send();
+  }
+
+  window.backend = {
+    Method: Method,
+    Url: Url,
+    load: load
+  };
+})();
